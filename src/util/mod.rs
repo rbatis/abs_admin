e1aaa008291d5ccbf280c7d0b308ddafef2d8f8f
zhuxiujia <zhuxiujia@qq.com>
2020-07-27 23:32:47 +0800

    edit password encoder

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,5 @@
-use std::hash::{Hash, Hasher};
 use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
 pub struct PasswordEncoder {}
 
@@ -7,10 +7,51 @@ impl PasswordEncoder {
     pub fn encode(raw_password: &str) -> String {
         let mut hasher = DefaultHasher::new();
         hasher.write(raw_password.as_bytes());
-        hasher.finish().to_string()
+        let result = hasher.finish().to_string();
+
+        let mut new_result = String::new();
+        for x in result.chars() {
+            match x {
+                '0' => {
+                    new_result.push('a');
+                }
+                '1' => {
+                    new_result.push('b');
+                }
+                '2' => {
+                    new_result.push('c');
+                }
+                '3' => {
+                    new_result.push('d');
+                }
+                '4' => {
+                    new_result.push('e');
+                }
+                '5' => {
+                    new_result.push('f');
+                }
+                '6' => {
+                    new_result.push('g');
+                }
+                '7' => {
+                    new_result.push('h');
+                }
+                '8' => {
+                    new_result.push('i');
+                }
+                '9' => {
+                    new_result.push('j');
+                }
+                _ => {
+                    new_result.push('0');
+                }
+            }
+        }
+
+        new_result
     }
     pub fn verify(password: &str, raw_password: &str) -> bool {
-        let hashed= PasswordEncoder::encode(raw_password);
+        let hashed = PasswordEncoder::encode(raw_password);
         password.eq(&hashed)
     }
 }
