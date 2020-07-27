b96a7519d6b5b42b7912768abaead7ef8a9d157f
zhuxiujia <zhuxiujia@qq.com>
2020-07-27 16:42:56 +0800

    add sign in

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,9 +1,9 @@
 use crypto::bcrypt::bcrypt;
 use std::fmt::Write;
 
-pub struct PasswordEncoder {}
+pub struct BCryptPasswordEncoder {}
 
-impl PasswordEncoder {
+impl BCryptPasswordEncoder {
     pub fn encode(raw_password: &str) -> String {
         let mut output = [0u8; 24];
         let salt = vec![0x10u8, 0x41u8, 0x04u8, 0x10u8, 0x41u8, 0x04u8, 0x10u8, 0x41u8, 0x04u8, 0x10u8, 0x41u8, 0x04u8, 0x10u8, 0x41u8, 0x04u8, 0x10u8];
@@ -16,13 +16,13 @@ impl PasswordEncoder {
         return output_password;
     }
     pub fn verify(password: &str, raw_password: &str) -> bool {
-        let s = PasswordEncoder::encode(raw_password);
+        let s = BCryptPasswordEncoder::encode(raw_password);
         password.eq(&s)
     }
 }
 
 #[test]
 fn test_encode() {
-    let s = PasswordEncoder::encode("123456");
+    let s = BCryptPasswordEncoder::encode("123456");
     println!("{}", s);
 }
