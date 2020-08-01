108922d5a8f966476663566eec0ed1d7514e054f
zhuxiujia <zhuxiujia@qq.com>
2020-08-01 15:41:38 +0800

    rename table

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -7,16 +7,16 @@ use cache_service::CacheService;
 use sys_res_service::SysResService;
 use sys_user_service::SysUserService;
 use crate::config::BOOT_CONFIG;
-use sys_role_service::SysSYS_ROLE_SERVICE;
+use sys_role_service::SysRoleService;
 
 
 
 
 lazy_static! {
    ///chache
-   pub static ref CACHE_SERVICE:CacheService = CacheService::new(&BOOT_CONFIG.redis_url);
+   pub static ref CACHE_SERVICE: CacheService = CacheService::new(&BOOT_CONFIG.redis_url);
    ///sys services
-   pub static ref SYS_RES_SERVICE:SysResService = SysResService{};
-   pub static ref SYS_USER_SERVICE:SysUserService = SysUserService{};
-   pub static ref SYS_ROLE_SERVICE:SysSYS_ROLE_SERVICE = SysSYS_ROLE_SERVICE{};
+   pub static ref SYS_RES_SERVICE: SysResService = SysResService{};
+   pub static ref SYS_USER_SERVICE: SysUserService = SysUserService{};
+   pub static ref SYS_ROLE_SERVICE: SysRoleService = SysRoleService{};
 }
--- src/service/sys_role_service.rs
+++ src/service/sys_role_service.rs
@@ -3,9 +3,9 @@ use crate::domain::domain::SysRole;
 use crate::domain::dto::RolePageDTO;
 
 ///角色服务
-pub struct SysSYS_ROLE_SERVICE {}
+pub struct SysRoleService {}
 
-impl SysSYS_ROLE_SERVICE {
+impl SysRoleService {
 
     pub async fn page(&self,arg: &RolePageDTO) -> Page<SysRole> {
         unimplemented!()
