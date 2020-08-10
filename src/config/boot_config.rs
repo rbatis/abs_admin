eb83680c39ca8ea4a7cfc0de5b9c6cfed1324a75
zhuxiujia <zhuxiujia@qq.com>
2020-08-10 19:37:14 +0800

    add boot.yml

--- src/config/boot_config.rs
+++ src/config/boot_config.rs
@@ -1,3 +1,8 @@
+use std::fs::File;
+use std::io::Read;
+
+use yaml_rust::{Yaml, YamlLoader};
+
 ///服务启动配置
 pub struct BootConfig {
     ///当前服务地址
@@ -13,11 +18,39 @@ pub struct BootConfig {
 ///默认配置
 impl Default for BootConfig {
     fn default() -> Self {
+        let mut f = File::open("src/boot.yml").expect("boot.yml not exist!");
+        let mut data = String::new();
+        f.read_to_string(&mut data);
+
+        let docs = YamlLoader::load_from_str(&data).unwrap();
+        println!("docs: {:?}", docs);
+
+        let server_url= get_cfg(&docs, "server_url").expect("boot.yml/server_url not exist!");
+        let log_path= get_cfg(&docs, "log_path").expect("boot.yml/log_path not exist!");
+        let redis_url= get_cfg(&docs, "redis_url").expect("boot.yml/redis_url not exist!");
+        let mysql_url= get_cfg(&docs, "mysql_url").expect("boot.yml/mysql_url not exist!");
+
         Self {
-            server_url: "127.0.0.1:8000".to_string(),
-            log_path: "requests.log".to_string(),
-            redis_url: "redis://127.0.0.1:6379".to_string(),
-            mysql_url: "mysql://root:123456@localhost:3306/test".to_string(),
+            server_url: server_url.as_str().unwrap_or("").to_string(),
+            log_path: log_path.as_str().unwrap_or("").to_string(),
+            redis_url: redis_url.as_str().unwrap_or("").to_string(),
+            mysql_url: mysql_url.as_str().unwrap_or("").to_string(),
         }
     }
+}
+
+
+fn get_cfg<'a>(docs: &'a Vec<Yaml>, key: &str) -> Option<&'a Yaml> {
+    for x in docs {
+        match x {
+            Yaml::Hash(hash) => {
+                let v = hash.get(&Yaml::String(key.to_string()));
+                if v.is_some() {
+                    return v;
+                }
+            }
+            _ => {}
+        }
+    }
+    return Some(&Yaml::Null);
 }
