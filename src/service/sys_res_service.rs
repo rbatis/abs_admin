9b5f3bffe47894e602887194215ee8ebba3554ac
zhuxiujia <zhuxiujia@qq.com>
2020-08-21 03:13:06 +0800

    auto type verdict

--- src/service/sys_res_service.rs
+++ src/service/sys_res_service.rs
@@ -14,8 +14,8 @@ pub struct SysResService {}
 impl SysResService {
     pub async fn page(&self,arg: &ResPageDTO) -> rbatis_core::Result<Page<SysRes>> {
         let page_req = PageRequest::new(arg.page.unwrap_or(1), arg.size.unwrap_or(10));
-        let data: rbatis_core::Result<Page<SysRes>> = RB.fetch_page_by_wrapper("", &RB.new_wrapper(), &page_req).await;
-        data
+        let data = RB.fetch_page_by_wrapper("", &RB.new_wrapper(), &page_req).await?;
+        Ok(data)
     }
 
     pub async fn save(&self,arg:&SysRes)->rbatis_core::Result<u64>{
