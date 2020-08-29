6f32ed84d39067e4d2dc3fc223e7b65a81780338
zhuxiujia <zhuxiujia@qq.com>
2020-08-30 02:41:08 +0800

    edit

--- src/config/app_config.rs
+++ src/config/app_config.rs
@@ -25,10 +25,11 @@ impl Default for ApplicationConfig {
             .read_to_string(&mut yml_data);
 
         let docs = YamlLoader::load_from_str(&yml_data).unwrap();
-        let server_url= get_cfg(&docs, "server_url").expect("application.yml/ server_url not exist!");
-        let log_path= get_cfg(&docs, "log_path").expect("application.yml/ log_path not exist!");
-        let redis_url= get_cfg(&docs, "redis_url").expect("application.yml/ redis_url not exist!");
-        let mysql_url= get_cfg(&docs, "mysql_url").expect("application.yml/ mysql_url not exist!");
+        //读取配置
+        let server_url= get_cfg(&docs, "server_url");
+        let log_path= get_cfg(&docs, "log_path");
+        let redis_url= get_cfg(&docs, "redis_url");
+        let mysql_url= get_cfg(&docs, "mysql_url");
 
         Self {
             server_url: server_url.as_str().unwrap_or("").to_string(),
@@ -40,17 +41,19 @@ impl Default for ApplicationConfig {
 }
 
 
-fn get_cfg<'a>(docs: &'a Vec<Yaml>, key: &str) -> Option<&'a Yaml> {
+/// 获取配置
+/// key: 需要获取配置的key
+fn get_cfg<'a>(docs: &'a Vec<Yaml>, key: &str) -> &'a Yaml {
     for x in docs {
         match x {
             Yaml::Hash(hash) => {
                 let v = hash.get(&Yaml::String(key.to_string()));
                 if v.is_some() {
-                    return v;
+                    return v.unwrap();
                 }
             }
             _ => {}
         }
     }
-    return Some(&Yaml::Null);
+    panic!(format!("application.yml/ {} not exist!",key))
 }
