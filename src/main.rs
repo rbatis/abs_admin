f1e8be945121b1946a0edc4dc3a66e2b0f772c65
ce <zhuxiujia@qq.com>
2020-10-30 10:17:24 +0800

    log filter

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,8 @@ use dao::RB;
 use crate::controller::{res_controller, role_controller, user_controller};
 use fast_log::plugin::file::FileAppender;
 use fast_log::plugin::console::ConsoleAppender;
-use fast_log::filter::ModuleFilter;
+use fast_log::filter::{ModuleFilter, NoFilter};
+use fast_log::fast_log::LogAppender;
 
 
 async fn index() -> impl Responder {
@@ -30,8 +31,23 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    //日志
-    fast_log::init_log("requests.log", 1000,log::Level::Info,CONFIG.debug);
+    //日志追加器
+    let mut appenders: Vec<Box<dyn LogAppender>> = vec![
+        Box::new(FileAppender::new("requests.log"))
+    ];
+    if CONFIG.debug {
+        appenders.push(Box::new(ConsoleAppender {}));
+    }
+    //自定义日志过滤
+    fast_log::init_custom_log(appenders, 1000, log::Level::Info, Box::new(
+        //NoFilter{}
+        ModuleFilter{ contains: vec!["rbatis".to_string(),
+                                     "actix".to_string(),
+                                     "crate".to_string(),
+                                     "abs_admin".to_string()] }
+    ));
+
+
     //ORM
     RB.link(&CONFIG.mysql_url).await.unwrap();
     //http路由
