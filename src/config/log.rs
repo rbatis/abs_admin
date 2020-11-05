5cad81d2ce6913644781f176db7ec99bcd65a35b
zhuxiujia <zhuxiujia@qq.com>
2020-11-05 19:59:02 +0800

    new version rbatis

--- src/config/log.rs
+++ src/config/log.rs
@@ -3,11 +3,12 @@ use fast_log::plugin::file::FileAppender;
 use fast_log::plugin::console::ConsoleAppender;
 use fast_log::filter::ModuleFilter;
 use crate::config::CONFIG;
+use fast_log::plugin::file_split::FileSplitAppender;
 
 pub fn init_log() {
     //自定义日志追加器
     let mut appenders: Vec<Box<dyn LogAppender>> = vec![
-        Box::new(FileAppender::new("requests.log"))
+        Box::new(FileSplitAppender::new("target/log/",1000000,true))
     ];
     if CONFIG.debug {
         appenders.push(Box::new(ConsoleAppender {}));
