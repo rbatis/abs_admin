7e2c67c27be5ca4f819a7fe28cb7ad8824828361
zhuxiujie <zhuxiujia@qq.com>
2020-12-16 12:54:14 +0800

    up fast_log

--- src/config/log.rs
+++ src/config/log.rs
@@ -4,11 +4,12 @@ use fast_log::plugin::console::ConsoleAppender;
 use fast_log::plugin::file_split::FileSplitAppender;
 
 use crate::config::CONFIG;
+use fast_log::consts::LogSize;
 
 pub fn init_log() {
     //自定义日志追加器
     let mut appenders: Vec<Box<dyn LogAppender>> = vec![
-        Box::new(FileSplitAppender::new("target/logs/", 1000000, true))
+        Box::new(FileSplitAppender::new("target/logs/", LogSize::MB(10), true))
     ];
     if CONFIG.debug {
         appenders.push(Box::new(ConsoleAppender {}));
