eb3bb6ae1e184c1dde78764a5e7bdd03bf365ed5
zhuxiujia <zhuxiujia@qq.com>
2020-12-16 23:19:02 +0800

    fix

--- src/config/log.rs
+++ src/config/log.rs
@@ -9,7 +9,7 @@ use fast_log::consts::LogSize;
 pub fn init_log() {
     //自定义日志追加器
     let mut appenders: Vec<Box<dyn LogAppender>> = vec![
-        Box::new(FileSplitAppender::new("target/logs/", LogSize::MB(10), true))
+        Box::new(FileSplitAppender::new("target/logs/", LogSize::MB(100), true))
     ];
     if CONFIG.debug {
         appenders.push(Box::new(ConsoleAppender {}));
