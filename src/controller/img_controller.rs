10eebab72642afe473cbb2758522b20e4ae4c94c
zhuxiujia <zhuxiujia@qq.com>
2021-01-01 22:44:53 +0800

    fix

--- src/controller/img_controller.rs
+++ src/controller/img_controller.rs
@@ -4,6 +4,8 @@ use captcha::filters::{Dots, Noise, Wave};
 
 use crate::domain::dto::CatpchaDTO;
 use crate::service::REDIS_SERVICE;
+use crate::domain::vo::RespVO;
+use crate::config::CONFIG;
 
 ///图形验证码接口
 /// 请求方式 GET
@@ -23,8 +25,14 @@ pub async fn captcha(arg: web::Query<CatpchaDTO>) -> impl Responder {
     let captcha_str = captcha.chars_as_string().to_lowercase();
     println!("account:{},captcha:{}", arg.account.as_ref().unwrap_or(&"".to_string()), &captcha_str);
     if arg.account.is_some() {
-        let result=REDIS_SERVICE.set_string(&format!("captch:account_{}", &arg.account.as_ref().unwrap()), captcha_str.as_str()).await;
-        println!("{:?}",result);
+        let result = REDIS_SERVICE.set_string(&format!("captch:account_{}", &arg.account.as_ref().unwrap()), captcha_str.as_str()).await;
+        println!("{:?}", result);
+        if CONFIG.debug == false {
+            //release mode, return the error
+            if result.is_err() {
+                return RespVO::from_result(&result).resp();
+            }
+        }
     }
     HttpResponse::Ok().content_type("image/png").body(png)
 }
