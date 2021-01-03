fbd08e7ab57d684052a0c7684c3c152c7c0ad31b
zhuxiujia <zhuxiujia@qq.com>
2021-01-03 20:04:20 +0800

    fix

--- src/config/log.rs
+++ src/config/log.rs
@@ -1,27 +1,16 @@
-use fast_log::appender::{LogAppender, FastLogFormatRecord};
-use fast_log::filter::ModuleFilter;
-use fast_log::plugin::console::ConsoleAppender;
-use fast_log::plugin::file_split::FileSplitAppender;
-
 use crate::config::CONFIG;
 use fast_log::consts::LogSize;
 use fast_log::plugin::file_split::RollingType;
 
 pub fn init_log() {
-    //自定义日志追加器
-    let mut appenders: Vec<Box<dyn LogAppender>> = vec![
-        Box::new(FileSplitAppender::new("target/logs/", LogSize::MB(100), RollingType::KeepNum(20), true, 1))
-    ];
-    //非Debug模式不输出控制台
-    if CONFIG.debug {
-        appenders.push(Box::new(ConsoleAppender {}));
-    }
-    //自定义日志过滤
-    fast_log::init_custom_log(appenders,
-                              1000,
-                              log::Level::Info,
-                              Box::new(ModuleFilter::new_exclude(vec!["sqlx".to_string()])),
-                              Box::new(FastLogFormatRecord {}));
+    fast_log::init_split_log("target/logs/",
+                             1000,
+                             LogSize::MB(100),
+                             true,
+                             RollingType::KeepNum(20),
+                             log::Level::Info,
+                             None,
+                             CONFIG.debug);
 }
 
 
