49eb3671958da61e7e1013a00616bdbaaf174934
zxj_pc <zhuxiujia@qq.com>
2021-01-31 01:12:41 +0800

    remove http client

--- src/util/client.rs
+++ src/util/client.rs
@@ -2,6 +2,8 @@
 mod test {
     #[async_std::test]
     pub async fn test_client() {
-
+        let uri = "https://www.baidu.com";
+        let string: String = surf::get(uri).recv_string().await.unwrap();
+        println!("{}", string);
     }
 }
