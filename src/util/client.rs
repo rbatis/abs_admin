352fd2d0db5eb92cb4080353937105e908f1ee29
zxj_pc <zhuxiujia@qq.com>
2021-01-31 01:05:39 +0800

    remove http client

--- src/util/client.rs
+++ src/util/client.rs
@@ -1,11 +1,7 @@
 #[cfg(test)]
 mod test {
-    use std::collections::HashMap;
-
     #[async_std::test]
     pub async fn test_client() {
-        let resp = reqwest::get("http://www.baidu.com")
-            .await.unwrap().bytes().await.unwrap();
-        println!("{:#?}", String::from_utf8(resp.to_vec()));
+
     }
 }
