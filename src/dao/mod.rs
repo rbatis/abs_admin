edb561e7631a069450cb7d2f19933a74bfc34723
zxj_pc <zhuxiujia@qq.com>
2021-01-30 21:28:47 +0800

    clear code

--- src/dao/mod.rs
+++ src/dao/mod.rs
@@ -12,28 +12,3 @@ lazy_static! {
      return rbatis;
   };
 }
-
-///测试
-#[cfg(test)]
-mod test {
-    use serde_json::json;
-
-    use crate::config::CONFIG;
-    use crate::dao::RB;
-
-    #[async_std::test]
-    async fn test_rbatis() {
-        fast_log::init_log("requests.log", 1000, log::Level::Info, None, true).unwrap();
-        RB.link(&CONFIG.mysql_url).await.unwrap();
-        let arg = &vec![json!(1)];
-        let v: serde_json::Value = RB
-            .fetch_prepare(
-                "",
-                "SELECT count(1) FROM biz_activity where delete_flag = ?;",
-                arg,
-            )
-            .await
-            .unwrap();
-        println!("{}", v.to_string());
-    }
-}
