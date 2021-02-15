efecbaa5258012940f228f531a44e2013f3f5aa2
zhuxiujie <zhuxiujia@qq.com>
2021-02-15 19:53:35 +0800

    reset to actix-web 3.3

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use abs_admin::controller::{
     sys_user_role_controller,
 };
 use abs_admin::dao::RB;
-//use abs_admin::middleware::auth::Auth;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use log::info;
 
@@ -26,7 +25,7 @@ async fn main() -> std::io::Result<()> {
     //路由
     HttpServer::new(|| {
         App::new()
-           // .wrap(Auth)
+            .wrap(abs_admin::middleware::auth::Auth)
             .route("/", web::get().to(index))
             .route("/api/sys_login", web::post().to(sys_user_controller::login))
             .route(
--- src/middleware/auth.rs
+++ src/middleware/auth.rs
@@ -1,79 +1,79 @@
-// use std::cell::RefCell;
-// use std::pin::Pin;
-// use std::rc::Rc;
-// use std::task::{Context, Poll};
-//
-// use crate::domain::vo::RespVO;
-// use actix_http::http::HeaderName;
-// use actix_web::body::MessageBody;
-// use actix_web::dev::{Service, ServiceRequest, ServiceResponse, Transform};
-// use actix_web::http::HeaderValue;
-// use actix_web::{error, Error};
-// use futures::future::{ok, Ready};
-// use futures::Future;
-//
-// // custom request auth middleware
-// pub struct Auth;
-//
-// impl<S, B> Transform<S> for Auth
-// where
-//     S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
-//     S::Future: 'static,
-//     B: MessageBody + 'static,
-// {
-//     type Request = ServiceRequest;
-//     type Response = ServiceResponse<B>;
-//     type Error = Error;
-//     type Transform = AuthMiddleware<S>;
-//     type InitError = ();
-//     type Future = Ready<Result<Self::Transform, Self::InitError>>;
-//
-//     fn new_transform(&self, service: S) -> Self::Future {
-//         ok(AuthMiddleware {
-//             service: Rc::new(RefCell::new(service)),
-//         })
-//     }
-// }
-//
-// pub struct AuthMiddleware<S> {
-//     service: Rc<RefCell<S>>,
-// }
-//
-// impl<S, B> Service for AuthMiddleware<S>
-// where
-//     S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
-//     S::Future: 'static,
-//     B: MessageBody + 'static,
-// {
-//     type Request = ServiceRequest;
-//     type Response = ServiceResponse<B>;
-//     type Error = Error;
-//     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
-//
-//     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-//         self.service.poll_ready(cx)
-//     }
-//
-//     fn call(&mut self, req: ServiceRequest) -> Self::Future {
-//         let mut svc = self.service.clone();
-//
-//         Box::pin(async move {
-//             let value = HeaderValue::from_str("").unwrap();
-//             let token = req.headers().get("access_token").unwrap_or(&value);
-//             if true {
-//                 //TODO token.len() > 0  && req.path().to_string().contains("/login")
-//                 let resp = svc.call(req).await?;
-//                 Ok(resp)
-//             } else {
-//                 let resp: RespVO<String> = RespVO {
-//                     code: Some("-1".to_string()),
-//                     msg: Some("Unauthorized".to_string()),
-//                     data: None,
-//                 };
-//                 Err(error::ErrorUnauthorized(
-//                     serde_json::json!(&resp).to_string(),
-//                 ))
-//             }
-//         })
-//     }
-// }
+use std::cell::RefCell;
+use std::pin::Pin;
+use std::rc::Rc;
+use std::task::{Context, Poll};
+
+use crate::domain::vo::RespVO;
+use actix_http::http::HeaderName;
+use actix_web::body::MessageBody;
+use actix_web::dev::{Service, ServiceRequest, ServiceResponse, Transform};
+use actix_web::http::HeaderValue;
+use actix_web::{error, Error};
+use futures::future::{ok, Ready};
+use futures::Future;
+
+// custom request auth middleware
+pub struct Auth;
+
+impl<S, B> Transform<S> for Auth
+where
+    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
+    S::Future: 'static,
+    B: MessageBody + 'static,
+{
+    type Request = ServiceRequest;
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Transform = AuthMiddleware<S>;
+    type InitError = ();
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ok(AuthMiddleware {
+            service: Rc::new(RefCell::new(service)),
+        })
+    }
+}
+
+pub struct AuthMiddleware<S> {
+    service: Rc<RefCell<S>>,
+}
+
+impl<S, B> Service for AuthMiddleware<S>
+where
+    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
+    S::Future: 'static,
+    B: MessageBody + 'static,
+{
+    type Request = ServiceRequest;
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: ServiceRequest) -> Self::Future {
+        let mut svc = self.service.clone();
+
+        Box::pin(async move {
+            let value = HeaderValue::from_str("").unwrap();
+            let token = req.headers().get("access_token").unwrap_or(&value);
+            if true {
+                //TODO token.len() > 0  && req.path().to_string().contains("/login")
+                let resp = svc.call(req).await?;
+                Ok(resp)
+            } else {
+                let resp: RespVO<String> = RespVO {
+                    code: Some("-1".to_string()),
+                    msg: Some("Unauthorized".to_string()),
+                    data: None,
+                };
+                Err(error::ErrorUnauthorized(
+                    serde_json::json!(&resp).to_string(),
+                ))
+            }
+        })
+    }
+}
