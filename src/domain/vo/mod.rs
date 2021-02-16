7ccaccaf512d85cdc2b663c060a163c1fe57d3ec
zhuxiujie <zhuxiujia@qq.com>
2021-02-16 23:49:13 +0800

    edit

--- src/domain/vo/mod.rs
+++ src/domain/vo/mod.rs
@@ -21,6 +21,10 @@ use rbatis::core::Error;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 
+
+pub const CODE_SUCCESS: &str = "SUCCESS";
+pub const CODE_FAIL: &str = "FAIL";
+
 /// http接口返回模型结构，提供基础的 code，msg，data 等json数据结构
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct RespVO<T> {
@@ -36,13 +40,13 @@ where
     pub fn from_result(arg: &Result<T, Error>) -> Self {
         if arg.is_ok() {
             Self {
-                code: Some("SUCCESS".to_string()),
+                code: Some(CODE_SUCCESS.to_string()),
                 msg: None,
                 data: arg.clone().ok(),
             }
         } else {
             Self {
-                code: Some("FAIL".to_string()),
+                code: Some(CODE_FAIL.to_string()),
                 msg: Some(arg.clone().err().unwrap().to_string()),
                 data: None,
             }
@@ -51,7 +55,7 @@ where
 
     pub fn from(arg: &T) -> Self {
         Self {
-            code: Some("SUCCESS".to_string()),
+            code: Some(CODE_SUCCESS.to_string()),
             msg: None,
             data: Some(arg.clone()),
         }
@@ -60,7 +64,7 @@ where
     pub fn from_error(code: &str, arg: &Error) -> Self {
         let mut code_str = code.to_string();
         if code_str.is_empty() {
-            code_str = "FAIL".to_string();
+            code_str = CODE_FAIL.to_string();
         }
         Self {
             code: Some(code_str),
@@ -72,7 +76,7 @@ where
     pub fn from_error_info(code: &str, info: &str) -> Self {
         let mut code_str = code.to_string();
         if code_str.is_empty() {
-            code_str = "FAIL".to_string();
+            code_str = CODE_FAIL.to_string();
         }
         Self {
             code: Some(code_str),
