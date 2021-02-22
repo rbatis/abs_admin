1a2139c779e925cd1a03219e29394a13867bf6a2
zhuxiujie <zhuxiujia@qq.com>
2021-02-22 12:32:42 +0800

    fix

--- src/service/sys_res_service.rs
+++ src/service/sys_res_service.rs
@@ -52,9 +52,9 @@ impl SysResService {
 
     ///添加资源
     pub async fn add(&self, arg: &SysRes) -> Result<u64> {
-        let old: Option<SysRes> = CONTEXT
+        let old: Vec<SysRes> = CONTEXT
             .rbatis
-            .fetch_by_wrapper(
+            .fetch_list_by_wrapper(
                 "",
                 &CONTEXT
                     .rbatis
@@ -62,7 +62,7 @@ impl SysResService {
                     .eq("permission", &arg.permission),
             )
             .await?;
-        if old.is_some() {
+        if old.len() > 0 {
             return Err(Error::from("权限已存在!"));
         }
         Ok(CONTEXT.rbatis.save("", arg).await?.rows_affected)
