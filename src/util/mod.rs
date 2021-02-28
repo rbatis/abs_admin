3a0900d0dc42b1e4b6aecec5c5a1417756b89267
zxj_pc <zhuxiujia@qq.com>
2021-03-01 03:43:04 +0800

    fix

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -11,13 +11,13 @@ pub mod string;
 ///         pub role_id:String
 ///      }
 ///      let user_roles: Vec<SysUserRole>;
-///      let role_ids = field_vec!(&user_roles,role_id); // role_ids: Vec<String>
+///      let role_ids = make_field_vec!(&user_roles,role_id); // role_ids: Vec<String>
 ///
 ///
 ///
 #[allow(unused_macros)]
 #[macro_export]
-macro_rules! field_vec {
+macro_rules! make_field_vec {
     ($vec_ref:expr,$field_name:ident) => {{
         let mut ids = vec![];
         for item in $vec_ref {
@@ -32,9 +32,21 @@ macro_rules! field_vec {
     }};
 }
 
+/// 取目标Vec的成员属性hashmap集合
+/// vec_ref: vec的引用，field_name: 结构体的field名称
+///
+/// for example:
+///      struct SysUserRole{
+///         pub role_id:String
+///      }
+///      let user_roles: Vec<SysUserRole>;
+///      let role_ids = make_field_map!(&user_roles,role_id); // role_ids: HashMap<String,SysUserRole>
+///
+///
+///
 #[allow(unused_macros)]
 #[macro_export]
-macro_rules! make_hash_map {
+macro_rules! make_field_map {
     ($vec_ref:expr,$field_name:ident) => {{
         let mut ids = std::collections::HashMap::new();
         for item in $vec_ref {
