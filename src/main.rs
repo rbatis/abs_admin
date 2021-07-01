1678debd2036b070727b4f1440587878e23646fb
zhuxiujia@qq.com <zhuxiujia@qq.com>
2021-07-02 02:04:10 +0800

    use /admin

--- src/main.rs
+++ src/main.rs
@@ -25,71 +25,71 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(abs_admin::middleware::auth::Auth)
             .route("/", web::get().to(index))
-            .route("/api/sys_login", web::post().to(sys_user_controller::login))
+            .route("/admin/sys_login", web::post().to(sys_user_controller::login))
             .route(
-                "/api/sys_user_info",
+                "/admin/sys_user_info",
                 web::post().to(sys_user_controller::info),
             )
             .route(
-                "/api/sys_user_detail",
+                "/admin/sys_user_detail",
                 web::post().to(sys_user_controller::detail),
             )
             //TODO .route("/sys_log_out", web::post().to(user_controller::log_out))
             .route(
-                "/api/sys_res_update",
+                "/admin/sys_res_update",
                 web::post().to(sys_res_controller::update),
             )
             .route(
-                "/api/sys_res_remove",
+                "/admin/sys_res_remove",
                 web::post().to(sys_res_controller::remove),
             )
-            .route("/api/sys_res_add", web::post().to(sys_res_controller::add))
+            .route("/admin/sys_res_add", web::post().to(sys_res_controller::add))
             .route(
-                "/api/sys_res_page",
+                "/admin/sys_res_page",
                 web::post().to(sys_res_controller::page),
             )
-            .route("/api/sys_res_all", web::post().to(sys_res_controller::all))
+            .route("/admin/sys_res_all", web::post().to(sys_res_controller::all))
             .route(
-                "/api/sys_res_layer_top",
+                "/admin/sys_res_layer_top",
                 web::post().to(sys_res_controller::layer_top),
             )
             .route(
-                "/api/sys_user_add",
+                "/admin/sys_user_add",
                 web::post().to(sys_user_controller::add),
             )
             .route(
-                "/api/sys_user_page",
+                "/admin/sys_user_page",
                 web::post().to(sys_user_controller::page),
             )
             .route(
-                "/api/sys_user_remove",
+                "/admin/sys_user_remove",
                 web::post().to(sys_user_controller::remove),
             )
             .route(
-                "/api/sys_user_update",
+                "/admin/sys_user_update",
                 web::post().to(sys_user_controller::update),
             )
             .route(
-                "/api/sys_role_add",
+                "/admin/sys_role_add",
                 web::post().to(sys_role_controller::add),
             )
             .route(
-                "/api/sys_role_update",
+                "/admin/sys_role_update",
                 web::post().to(sys_role_controller::update),
             )
             .route(
-                "/api/sys_role_delete",
+                "/admin/sys_role_delete",
                 web::post().to(sys_role_controller::remove),
             )
             .route(
-                "/api/sys_role_page",
+                "/admin/sys_role_page",
                 web::post().to(sys_role_controller::page),
             )
             .route(
-                "/api/sys_role_layer_top",
+                "/admin/sys_role_layer_top",
                 web::post().to(sys_role_controller::layer_top),
             )
-            .route("/api/captcha", web::get().to(img_controller::captcha))
+            .route("/admin/captcha", web::get().to(img_controller::captcha))
     })
     .bind(&CONTEXT.config.server_url)?
     .run()
