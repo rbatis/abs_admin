b573f5959f564a9136ae332ea50c5d361970d6fc
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-01-16 20:54:21 +0800

    edit unwrap_or_default

--- src/util/options.rs
+++ src/util/options.rs
@@ -4,7 +4,7 @@ pub trait OptionStringRefUnwrapOrDefault<T> {
     fn unwrap_or_default(&self) -> T;
 }
 
-static EmptyStr: Lazy<String> = Lazy::new(|| {
+static EMPTY_STR: Lazy<String> = Lazy::new(|| {
     String::new()
 });
 
@@ -12,7 +12,7 @@ static EmptyStr: Lazy<String> = Lazy::new(|| {
 impl<'a> OptionStringRefUnwrapOrDefault<&'a String> for Option<&'a String> {
     fn unwrap_or_default(&self) -> &'a String {
         return match self {
-            None => { &EmptyStr }
+            None => { &EMPTY_STR }
             Some(v) => {
                 v
             }
