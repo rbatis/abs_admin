ad42191eb835312513098ae2c2259072271be992
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-01-16 20:54:04 +0800

    edit unwrap_or_default

--- src/service/sys_user_service.rs
+++ src/service/sys_user_service.rs
@@ -269,7 +269,7 @@ impl SysUserService {
         sign_vo.permissions = self.loop_load_level_permission(&user_id, &all_res).await?;
         let jwt_token = JWTToken {
             id: user.id.clone().unwrap_or_default(),
-            account: user.account.clone().unwrap_or_default(),
+            account: user.account.unwrap_or_default(),
             permissions: sign_vo.permissions.clone(),
             role_ids: vec![],
             exp: DateTimeNative::now().timestamp_millis() as usize,
