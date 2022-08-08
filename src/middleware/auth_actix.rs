dba464cb43ead2d62ead51a485b5139712dd0376
zxj <zhuxiujia@qq.com>
2022-08-08 11:27:16 +0800

    add middle

--- src/middleware/auth_actix.rs
+++ src/middleware/auth_actix.rs
@@ -2,19 +2,23 @@ use std::{
     future::{ready, Ready},
     rc::Rc,
 };
-
 use actix_web::{
     dev::{self, Service, ServiceRequest, ServiceResponse, Transform},
     web::BytesMut,
     Error, HttpMessage,
 };
+use actix_web::dev::Response;
+use actix_web::error::ErrorUnauthorized;
 use futures_util::{future::LocalBoxFuture, stream::StreamExt};
+use crate::domain::vo::RespVO;
+use crate::middleware::auth::{check_auth, checked_token, is_white_list_api};
+use crate::service::CONTEXT;
 
 pub struct Auth;
 
 impl<S: 'static, B> Transform<S, ServiceRequest> for Auth
     where
-        S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+        S: Service<ServiceRequest, Response=ServiceResponse<B>, Error=Error>,
         S::Future: 'static,
         B: 'static,
 {
@@ -38,7 +42,7 @@ pub struct AuthMiddleware<S> {
 
 impl<S, B> Service<ServiceRequest> for AuthMiddleware<S>
     where
-        S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
+        S: Service<ServiceRequest, Response=ServiceResponse<B>, Error=Error> + 'static,
         S::Future: 'static,
         B: 'static,
 {
@@ -49,46 +53,11 @@ impl<S, B> Service<ServiceRequest> for AuthMiddleware<S>
     dev::forward_ready!(service);
 
     fn call(&self, mut req: ServiceRequest) -> Self::Future {
-        //let token = req.headers().get("access_token").map(|v|v.to_str().unwrap_or_default().to_string()).unwrap_or_default();
-        //                 let path = req.path().to_string();
-        //                 let fut = srv.call(req);
-        //                 Box::pin(async move {
-        //                     //debug mode not enable auth
-        //                     if !CONTEXT.config.debug{
-        //                         if !is_white_list_api(&path) {
-        //                             //非白名单检查token是否有效
-        //                             match checked_token(&token, &path).await {
-        //                                 Ok(data) => {
-        //                                     match check_auth(&data, &path).await {
-        //                                         Ok(_) => {}
-        //                                         Err(e) => {
-        //                                             //仅提示拦截
-        //                                             let resp: RespVO<String> = RespVO {
-        //                                                 code: Some("-1".to_string()),
-        //                                                 msg: Some(format!("无权限访问:{}", e.to_string())),
-        //                                                 data: None,
-        //                                             };
-        //                                             return Err(ErrorUnauthorized(serde_json::json!(&resp).to_string()));
-        //                                         }
-        //                                     }
-        //                                 }
-        //                                 Err(e) => {
-        //                                     //401 http状态码会强制前端退出当前登陆状态
-        //                                     let resp: RespVO<String> = RespVO {
-        //                                         code: Some("-1".to_string()),
-        //                                         msg: Some(format!("Unauthorized for:{}", e.to_string())),
-        //                                         data: None,
-        //                                     };
-        //                                     return Err(ErrorUnauthorized(serde_json::json!(&resp).to_string()));
-        //                                 }
-        //                             }
-        //                         }
-        //                     }
-        //                     let res = fut.await?;
-        //                     Ok(res)
-        //                 })
-
         let svc = self.service.clone();
+
+        let token = req.headers().get("access_token").map(|v| v.to_str().unwrap_or_default().to_string()).unwrap_or_default();
+        let path = req.path().to_string();
+        // let fut = srv.call(req);
 
         Box::pin(async move {
             let mut body = BytesMut::new();
@@ -96,11 +65,40 @@ impl<S, B> Service<ServiceRequest> for AuthMiddleware<S>
             while let Some(chunk) = stream.next().await {
                 body.extend_from_slice(&chunk?);
             }
-
-            println!("request body: {body:?}");
+            //debug mode not enable auth
+            if !CONTEXT.config.debug {
+                if !is_white_list_api(&path) {
+                    //非白名单检查token是否有效
+                    match checked_token(&token, &path).await {
+                        Ok(data) => {
+                            match check_auth(&data, &path).await {
+                                Ok(_) => {}
+                                Err(e) => {
+                                    //仅提示拦截
+                                    let resp: RespVO<String> = RespVO {
+                                        code: Some("-1".to_string()),
+                                        msg: Some(format!("无权限访问:{}", e.to_string())),
+                                        data: None,
+                                    };
+                                    return Err(ErrorUnauthorized(serde_json::json!(&resp).to_string()));
+                                    // let resp=resp.resp_json();
+                                    // return Ok(req.into_response(resp));
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            //401 http状态码会强制前端退出当前登陆状态
+                            let resp: RespVO<String> = RespVO {
+                                code: Some("-1".to_string()),
+                                msg: Some(format!("Unauthorized for:{}", e.to_string())),
+                                data: None,
+                            };
+                            return Err(ErrorUnauthorized(serde_json::json!(&resp).to_string()));
+                        }
+                    }
+                }
+            }
             let res = svc.call(req).await?;
-
-            println!("response: {:?}", res.headers());
             Ok(res)
         })
     }
