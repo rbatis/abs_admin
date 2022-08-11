24f137c59ee3c7b121b2aa74996bcfca89ad6906
zhuxiujia <zhuxiujia@qq.com>
2022-08-12 00:31:19 +0800

    fix bugs

--- src/domain/table/tables.rs
+++ src/domain/table/tables.rs
@@ -22,7 +22,7 @@ impl_select_page!(SysRes{select_page(dto: &crate::domain::dto::ResPageDTO) =>
     "`where del = 0 `
       if dto.name!=null && dto.name!= '':
          `and name like %#{dto.name}%`
-      ` and parent_id = null`
+      ` and parent_id IS NULL`
       ` order by create_date `"});
 impl_select!(SysRes{select_by_permission_or_name(permission:&str,name:&str) => "`where permission = #{permission} or name = #{name}`"});
 impl_delete!(SysRes{delete_by_parent_id(parent_id:&str) => "`where parent_id = #{parent_id}`"});
@@ -32,7 +32,7 @@ impl_select!(SysRes{select_by_ids(ids:&Vec<String>)=>
        for _,id in ids:
          #{id},
      `)`"});
-impl_select!(SysRes{select_by_parent_id_null()=>"`where parent_id = null order by create_date desc`"});
+impl_select!(SysRes{select_by_parent_id_null()=>"`where parent_id IS NULL order by create_date desc`"});
 
 
 ///角色表
@@ -61,7 +61,7 @@ impl_select_page!(SysRole{select_page_by_name(name:&str)=>
     "`where del = 0`
     if name != '':
       ` and name like %#{name}%`
-    ` and parent_id = null `
+    ` and parent_id IS NULL `
     if !sql.contains('count'):
      `order by create_date desc`"});
 
