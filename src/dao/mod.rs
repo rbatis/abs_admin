b2ce66d372c9416d39cd2f8caecf697809ec53cb
zxj <zhuxiujia@qq.com>
2022-08-16 11:36:46 +0800

    edit doc

--- src/dao/mod.rs
+++ src/dao/mod.rs
@@ -7,7 +7,7 @@ pub fn init_rbatis(config: &ApplicationConfig) -> Rbatis {
     let mut rbatis = Rbatis::new();
     if config.debug.eq(&false) && rbatis.is_debug_mode() {
         panic!(
-            r#"已使用release模式，但是rbatis仍使用debug模式！请删除 Cargo.toml 中 rbatis的配置 features = ["debug_mode"]"#
+            r#"已使用release模式，但是仍使用debug模式！请修改 application.yml 中debug配置 ，并删除 Cargo.toml 中 rbatis的配置 features = ["debug_mode"]"#
         );
     }
     return rbatis;
