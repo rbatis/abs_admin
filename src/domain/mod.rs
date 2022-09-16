a51f0fc97dd7f2926cf7f2d5388ce857a03ef434
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-09-17 01:38:51 +0800

    fix check

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -14,7 +14,7 @@ pub fn init_rbatis(config: &ApplicationConfig) -> Rbatis {
     let rbatis = Rbatis::new();
     if rbatis.is_debug_mode() == false && config.debug.eq(&true) {
         panic!(
-            r#"已使用release模式运行，但是仍使用debug模式！请修改 application.yml 中debug配置项为false！"#
+            r#"已使用release模式运行，但是仍使用debug模式！请修改 application.yml 中debug配置项为  debug: false"#
         );
     }
     return rbatis;
