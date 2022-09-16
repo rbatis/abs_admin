1689ffd5252b1c2639260b8058358f0afcc7e7c0
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-09-17 01:36:20 +0800

    fix check

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,23 +1,20 @@
 use crate::service::ApplicationConfig;
 use rbatis::Rbatis;
-
-/// * 数据传输层（dto，Data Transfer Object ）,存放接口传输的结构体
-pub mod dto;
 /// 数据库查询实现
 pub mod mapper;
-///DDD分层架构，分为
-///
-/// * 数据库结构,该层存放数据库结构体模型
+/// DDD分层架构，数据传输层（dto，Data Transfer Object ）,存放接口传输的结构体
+pub mod dto;
+/// DDD分层架构，数据库结构,该层存放数据库结构体模型
 pub mod table;
-/// * 展示层（vo，View Object），存放展示的结构体
+/// DDD分层架构，展示层（View Object），存放展示的结构体
 pub mod vo;
 
 ///实例化 rbatis orm 连接池
 pub fn init_rbatis(config: &ApplicationConfig) -> Rbatis {
     let rbatis = Rbatis::new();
-    if config.debug.eq(&false) && rbatis.is_debug_mode() {
+    if rbatis.is_debug_mode() == false && config.debug.eq(&true) {
         panic!(
-            r#"已使用release模式，但是仍使用debug模式！请修改 application.yml 中debug配置 ，并删除 Cargo.toml 中 rbatis的配置 features = ["debug_mode"]"#
+            r#"已使用release模式运行，但是仍使用debug模式！请修改 application.yml 中debug配置项为false！"#
         );
     }
     return rbatis;
