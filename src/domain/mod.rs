bfa5a7954156d9c13e6ebcc2a2e65ff7791e2614
zxj <zhuxiujia@qq.com>
2023-09-22 15:47:43 +0800

    add errors for #12

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -9,7 +9,7 @@ pub fn init_rbatis(config: &ApplicationConfig) -> RBatis {
     let rbatis = RBatis::new();
     if rbatis.is_debug_mode() == false && config.debug.eq(&true) {
         panic!(
-            r#"已使用release模式运行，但是仍使用debug模式！请修改 application.yml 中debug配置项为  debug: false"#
+            r#"please edit application.yml   “debug: false” "#
         );
     }
     return rbatis;
