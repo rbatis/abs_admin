bad99c1c7c58f9863b09286ce0a447c805497cfd
zxj <zhuxiujia@qq.com>
2023-11-02 16:46:04 +0800

    add database

--- build.rs
+++ build.rs
@@ -7,10 +7,8 @@ pub struct ApplicationConfig {
 }
 
 fn main() {
-    println!("start build");
     let js_data = include_str!("application.json5");
     let config: ApplicationConfig = json5::from_str(js_data).expect("load config file fail");
-    println!("config={:?}", config);
     let mut data = String::new();
     let mut f = OpenOptions::new().write(true).create(true).open("target/driver.rs").unwrap();
     _ = f.read_to_string(&mut data);
