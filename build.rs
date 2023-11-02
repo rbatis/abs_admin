c70dcbcc428f289d13e56d26643387c58ef0b83b
zxj <zhuxiujia@qq.com>
2023-11-02 16:22:54 +0800

    add database

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
-use std::io::Write;
+use std::fs::{OpenOptions};
+use std::io::{Read, Write};
 
 #[derive(Debug, PartialEq, serde::Serialize, serde::Deserialize)]
 pub struct ApplicationConfig {
@@ -9,10 +9,14 @@ pub struct ApplicationConfig {
 fn main() {
     println!("start build");
     let js_data = include_str!("application.json5");
-    let result: ApplicationConfig = json5::from_str(js_data).expect("load config file fail");
-    println!("result={:?}", result);
-    let mut f = File::create("target/driver.rs").unwrap();
-    f.write_all(result.database_struct.as_ref()).unwrap();
-    f.flush().unwrap();
-    drop(f);
+    let config: ApplicationConfig = json5::from_str(js_data).expect("load config file fail");
+    println!("config={:?}", config);
+    let mut data = String::new();
+    let mut f = OpenOptions::new().write(true).create(true).open("target/driver.rs").unwrap();
+    _ = f.read_to_string(&mut data);
+    if data.is_empty() || data!=config.database_struct{
+        _ = f.set_len(0);
+        f.write_all(config.database_struct.as_ref()).unwrap();
+        f.flush().unwrap();
+    }
 }
