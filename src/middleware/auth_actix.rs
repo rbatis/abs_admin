48eb4bb183371c0a7a77e325b9f21e400261ae01
zhuxiujia@qq.com <zhuxiujia@qq.com>
2024-02-24 20:24:43 +0800

    clear deadcode

--- src/middleware/auth_actix.rs
+++ src/middleware/auth_actix.rs
@@ -1,7 +1,6 @@
 use crate::domain::vo::RespVO;
 use crate::middleware::auth::{check_auth, checked_token, is_white_list_api};
 use crate::service::CONTEXT;
-use actix_http::body::BoxBody;
 use actix_web::error::ErrorUnauthorized;
 use actix_web::http::header::{HeaderName, HeaderValue};
 use actix_web::{
@@ -13,6 +12,7 @@ use std::{
     future::{ready, Ready},
     rc::Rc,
 };
+use actix_web::body::BoxBody;
 
 pub struct Auth;
 
