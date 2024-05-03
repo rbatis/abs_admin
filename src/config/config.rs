f131d372d375dae3f7e0b06696a80fabfea83de5
zxj <zhuxiujia@qq.com>
2024-05-04 01:10:27 +0800

    use code 0

--- src/config/config.rs
+++ src/config/config.rs
@@ -43,7 +43,7 @@ impl Default for ApplicationConfig {
             result.debug = false;
         }
         if result.debug {
-            println!("[abs_admin] load config:{:?}", result);
+            println!("[abs_admin] load config:{:#?}", result);
             println!("[abs_admin] ///////////////////// Start On Debug Mode ////////////////////////////");
         } else {
             println!("[abs_admin] ///////////////////// Start On Release Mode ////////////////////////////");
