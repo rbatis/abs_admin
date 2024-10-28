d85ac7faa978979e8a634606b9840be1f959e0d9
zxj <zhuxiujia@qq.com>
2024-10-28 15:09:04 +0800

    add service

--- src/service/file_service.rs
+++ src/service/file_service.rs
@@ -1,8 +1,9 @@
 use std::fmt::Debug;
+use futures_util::future::BoxFuture;
 use crate::error::Result;
 
 pub trait IFileService: Sync + Send + Debug {
-    fn upload(&self, name: &str, data: Vec<u8>) -> Result<()>;
-    fn download(&self, name: &str) -> Result<Vec<u8>>;
-    fn list(&self) -> Result<Vec<String>>;
+    fn upload(&self, name: &str, data: Vec<u8>) -> BoxFuture<Result<()>>;
+    fn download(&self, name: &str) -> BoxFuture<Result<Vec<u8>>>;
+    fn list(&self) -> BoxFuture<Result<Vec<String>>>;
 }
