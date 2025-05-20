0cd09875f58bd2ee6cc363f82c18b668fe133191
zxj <zhuxiujia@qq.com>
2025-05-21 02:10:33 +0800

    move to actix

--- src/main.rs
+++ src/main.rs
@@ -67,11 +67,6 @@ async fn main() -> std::io::Result<()> {
                     .route("/admin/auth/check", web::post().to(sys_auth_controller::check))
                     .wrap_fn(abs_admin::middleware::auth_actix::auth)
             )
-            // serve vue files
-            .default_service(web::route().to(|| async {
-                NamedFile::open("dist/index.html")
-            })
-            )
             .wrap(cors)
             .app_data(
                 web::PayloadConfig::default()
