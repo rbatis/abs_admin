0387d9c66204283141331d116d08dc9f2638ae84
zxj <zhuxiujia@qq.com>
2026-03-24 20:05:46 +0800

    fix mod

--- src/domain/table/mod.rs
+++ src/domain/table/mod.rs
@@ -1,6 +1,5 @@
 mod enums;
 pub mod rbac;
-pub mod sms;
 pub mod sys_dict;
 pub mod sys_user;
 mod tables_init;
--- src/domain/table/sms.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-
-#[derive(Serialize, Deserialize)]
-pub struct Sms {
-    pub account: String,
-    pub args: HashMap<String, String>,
-}
